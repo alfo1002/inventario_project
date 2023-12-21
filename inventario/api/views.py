3490b213aeb8b292612882b5dee77bfd7fe25eb6
alfo1002 <dodoy147@hotmail.com>
2023-12-21 08:23:06 -0500

    add: implementación de JWT con simplejwt

--- inventario/api/views.py
+++ inventario/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from inventario.models import Producto
 from inventario.api.serializer import ProductoSerializer
 
 class ProductoViewSet(viewsets.ModelViewSet):
+    #permission_classes = [IsAuthenticated]
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
--- inventario_project/settings.py
+++ inventario_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'inventario',
 ]
 
@@ -127,3 +128,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
